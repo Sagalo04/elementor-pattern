b44cfc0f70d7cdfb4e7c9a6c76d34e7ba71f5bc3
Sagalo04 <santiago.garcna@uao.edu.co>
2020-03-09 23:23:13 -0500

    MdP v1

---
+++ widgets/modulo_de_perfil_docente.php
@@ -0,0 +1,293 @@
+<?php
+namespace ElementorPatternUao\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager; 
+use Elementor\Utils; 
+
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Elementor Boton_De_Lista_Compuesto
+ *
+ * Elementor widget for Boton_De_Lista_Compuesto
+ *
+ * @since 1.0.0
+ */
+class Modulo_De_Perfil_Docente extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'modulo_de_perfil_docente';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Modulo_De_Perfil_Docente', 'modulo_de_perfil_docente' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-call-to-action';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'pattern-uao' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'general_section',
+			[
+				'label' => __( 'Contenido', 'modulo_de_perfil_docente' ),
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				'label' => __( 'Titulo', 'modulo_de_perfil_docente' ),
+				'type' => Controls_Manager::TEXT,
+			]
+        );
+        $this->add_control(
+			'name',
+			[
+				'label' => __( 'Nombre', 'modulo_de_perfil_docente' ),
+                'type' => Controls_Manager::TEXT,              
+			]
+        );
+        $this->add_control(
+			'charge',
+			[
+				'label' => __( 'Cargo', 'modulo_de_perfil_docente' ),
+				'type' => Controls_Manager::TEXT,
+			]
+        );
+        $this->add_control(
+			'phone',
+			[
+				'label' => __( 'Telefono', 'modulo_de_perfil_docente' ),
+				'type' => Controls_Manager::TEXT,
+			]
+        );
+        $this->add_control(
+			'mail',
+			[
+				'label' => __( 'Email', 'modulo_de_perfil_docente' ),
+                'type' => Controls_Manager::TEXT,              
+			]
+		);
+		
+        $this->add_control(
+			'color_title',
+			[
+				'label' => __( 'Colores Principales', 'modulo_de_perfil_docente' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => 'Rojo-principal-1',
+				'options' => [
+					'Rojo-principal-1'  => 'Rojo-principal-1',
+					'Negro' => 'Negro',
+					'Blanco' => 'Blanco',
+					'Rojo-2'  => 'Rojo-2',
+					'Rojo-3' => 'Rojo-3',
+					'Rojo-4' => 'Rojo-4',
+					'Rojo-5'  => 'Rojo-5',
+					'Rojo-6' => 'Rojo-6',
+					'Rojo-7' => 'Rojo-7',
+					'Gris-1'  => 'Gris-1',
+					'Gris-2' => 'Gris-2',
+					'Gris-3' => 'Gris-3',
+					'Gris-4' => 'Gris-4',
+					'Gris-5' => 'Gris-5',
+					'Rojizo-1'  => 'Rojizo-1',
+					'Rojizo-2' => 'Rojizo-2',
+					'Rojizo-3' => 'Rojizo-3',
+					'Rojizo-4'  => 'Rojizo-4',
+					'Morado-1'  => 'Morado-1',
+					'Morado-2' => 'Morado-2',
+					'Morado-3' => 'Morado-3',
+					'Morado-4'  => 'Morado-4',
+					'Azul-1'  => 'Azul-1',
+					'Azul-2' => 'Azul-2',
+					'Azul-3' => 'Azul-3',
+					'Azul-4'  => 'Azul-4',
+					'Verde-1'  => 'Verde-1',
+					'Verde-2' => 'Verde-2',
+					'Verde-3' => 'Verde-3',
+					'Verde-4'  => 'Verde-4',
+					'Naranja-1'  => 'Naranja-1',
+					'Naranja-2' => 'Naranja-2',
+					'Naranja-3' => 'Naranja-3',
+					'Naranja-4'  => 'Naranja-4',
+					'Verde-claro-1'  => 'Verde-claro-1',
+					'Verde-claro-2' => 'Verde-claro-2',
+					'Verde-claro-3' => 'Verde-claro-3',
+					'Verde-claro-4'  => 'Verde-claro-4',
+					'Amarillo-1'  => 'Amarillo-1',
+					'Amarillo-2' => 'Amarillo-2',
+					'Amarillo-3' => 'Amarillo-3',
+					'Amarillo-4'  => 'Amarillo-4',
+					'Naranja-sinapsis-1'  => 'Naranja-sinapsis-1',
+					'Naranja-sinapsis-2' => 'Naranja-sinapsis-2',
+					'Naranja-sinapsis-3' => 'Naranja-sinapsis-3',
+					'Naranja-sinapsis-4'  => 'Naranja-sinapsis-4',
+					'Purpura-sinapsis-1'  => 'Purpura-sinapsis-1',
+					'Purpura-sinapsis-2' => 'Purpura-sinapsis-2',
+					'Purpura-sinapsis-3' => 'Purpura-sinapsis-3',
+					'Purpura-sinapsis-4'  => 'Purpura-sinapsis-4',
+					'Gris-sinapsis-1'  => 'Gris-sinapsis-1',
+					'Gris-sinapsis-2' => 'Gris-sinapsis-2',
+					'Gris-sinapsis-3' => 'Gris-sinapsis-3',
+					'Gris-sinapsis-4'  => 'Gris-sinapsis-4',
+					
+				],
+			]
+		);
+		$this->end_controls_section();
+
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+        
+        echo '<article class="directory-card" style="margin:0 auto">';
+        echo '<header class="'.$settings['color_title'].'">';
+        echo '<h1>'.$settings['title'].'</h1>';
+        echo '</header>';
+        echo '<p>';
+        echo '<strong>Director</strong>';
+        echo '<br>';
+        echo  $settings['director'];
+        echo '</p>';
+        echo '<section class="set-icon">';
+        echo '<div class="circle-icon">';
+        echo '<span class="icon icon-phone"></span>';
+        echo '</div>';
+        echo $settings['phone'];
+        echo '</section>';
+        echo '<section class="set-icon">';
+        echo '<div class="circle-icon">';
+        echo '<span class="icon icon-envelope"></span>';
+        echo '</div>';
+        echo '<a href="mailto:'.$settings['mail'].'">'.$settings['mail'].'</a>';
+        echo '</section>';
+        echo '</article>';
+
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _content_template() {
+		?>
+
+        <div class="teacher-profile ">
+            <div class="faculty">
+                <p>Nuestro equipo Docente</p>
+                <p class="category-tag ">
+                 Universidad Autónoma de Occidente
+                </p>
+                <p>Departamento de Automática y Electrónica</p>
+            </div>
+
+            <div class="teacher">
+                <div class="photo-info">
+                    <figure>
+                         <img src="../../../images/example-image.jpg" alt="UAO">
+                    </figure>
+                    <div class="info">
+                         <p class="name">Diego Fernando Almario Álvarez</p>
+                         <p>Docente</p>
+                        <div class="phone">
+                            <div class="circle-icon">
+                                <span class="icon icon-phone"></span>
+                            </div>
+                            <p>318 8000 - Ext. 11346</p>
+                        </div>
+                        <div class="email">
+                            <div class="circle-icon">
+                                <span class="icon icon-envelope"></span>
+                            </div>
+                            <a href="">bdfcorreo@uao.edu.co</a>
+                        </div>
+                    </div>
+                </div>
+
+                <a class="list-btn" href="#">
+                    <div class="lb-text">
+                        Visita la lista completa de Docentes de la Facultad
+                    </div>
+                    <div class="lb-arrow">
+                        <span class="icon icon-chevron-right"></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+         
+		<?php
+	}
+}
